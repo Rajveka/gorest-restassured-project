b423bafd7e42ad9809051e52a8da88f04f0c4064
Rajveka <rajvekariya603@gmail.com>
2024-07-11 17:20:47 +0100

    My gorest-restassured-project

---
+++ src/test/java/com/gorest/testbase/TestBase.java
@@ -0,0 +1,16 @@
+package com.gorest.testbase;
+
+import com.gorest.utils.PropertyReader;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+
+public class TestBase {
+
+    @BeforeClass
+    public void inIt() {
+
+        RestAssured.baseURI = PropertyReader.getInstance().getProperty("baseUrl");  //with propertyreader
+        RestAssured.basePath = PropertyReader.getInstance().getProperty("basePath");  //with propertyreader
+
+    }
+}
